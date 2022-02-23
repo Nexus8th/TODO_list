19d076b0d638d19587f57c4455dc1c2f105dd5e0
Julien <mmos002@gmail.com>
2022-02-23 16:40:20 +0100

    JS-CSS wip

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ window.addEventListener('load', () => {
         const task = input.value;
         
         if (!task) {
+            
             return;
         }
         
@@ -18,9 +19,28 @@ window.addEventListener('load', () => {
 
         const task_content_el = document.createElement("div");
         task_content_el.classList.add("content");
-        task_content_el.innerHTML = task;
 
         task_el.appendChild(task_content_el);
+
+        const task_input_el = document.createElement("input");
+        task_input_el.classList.add("text");
+        task_input_el.type = "text";
+        task_input_el.value = task;
+        task_input_el.setAttribute("readonly", "readonly");
+
+        task_content_el.appendChild(task_input_el);
+
+        const task_actions_el = document.createElement("div");
+        task_actions_el.classList.add("actions");
+
+        const task_edit_el = document.createElement("button");
+        task_edit_el.classList.add("edit");
+        task_edit_el.innerHTML = "Editer"
+
+        const task_delete_el = document.createElement("button");
+        task_delete_el.classList.add("delete");
+        task_delete_el.innerHTML = "Supprimer"
+        console.log(task_edit_el, task_delete_el);
 
         list_el.appendChild(task_el);
     })
